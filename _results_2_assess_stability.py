034edc786d5a355043ff11299a8b51b26fe564e5
Susanne Weis <s.weis@fz-juelich.de>
2026-01-08 22:55:28 +0100

    working on results summary

--- _results_2_assess_stability.py
+++ _results_2_assess_stability.py
@@ -1,67 +1,88 @@
 import numpy as np
 import pandas as pd
 
-base_path =  "/Users/sweis/Data/Arbeit/Juseless/data/project/brainvar_sexdiff_movies" 
-proj = "v2"
+def main(base_path,res_path,nn_values):
 
-# Map sex to numbers
-sex_mapping = {"male": 1, "female": 2}
+    # Map sex to numbers
+    sex_mapping = {"male": 1, "female": 2}
 
-results_path = f"{base_path}/results_run_sTOPF_{proj}/results_nn{nn_mi}/ind_classification"
-results_out_path = f"{base_path}/results_run_sTOPF_{proj}"
+    true_sex_vecs = {}
+    pred_sex_vecs = {}
 
-true_sex_vecs = {}
-pred_sex_vecs = {}
+    for nn_mi in nn_values:
 
-for nn_mi in range(1, 11):
-    ind_class_name = f"classification_subjects_movies_nn{nn_mi}_top_100perc.csv"
-    ind_class_file =  f"{results_path}/{ind_class_name}"
+        results_path = f"{base_path}/{res_path}/results_nn{nn_mi}/ind_classification"
+        results_out_path = f"{base_path}/{res_path}"
+        
+        ind_class_name = f"classification_subjects_movies_nn{nn_mi}_top_100perc.csv"
+        ind_class_file =  f"{results_path}/{ind_class_name}"
 
-    # Read CSV
-    ind_class = pd.read_csv(ind_class_file)
+        # Read CSV
+        ind_class = pd.read_csv(ind_class_file)
 
-    # Sort by movie, then subject
-    ind_class = ind_class.sort_values(["movie", "subject"])
+        # Sort by movie, then subject
+        ind_class = ind_class.sort_values(["movie", "subject"])
 
-    # sex as numbers and the predicted sex as numbers:
-    ind_class["sex_num"] = ind_class["sex"].map(sex_mapping)
-    ind_class["class_num"] = ind_class["classification"].map(sex_mapping)
+        # sex as numbers and the predicted sex as numbers:
+        ind_class["sex_num"] = ind_class["sex"].map(sex_mapping)
+        ind_class["class_num"] = ind_class["classification"].map(sex_mapping)
 
-    true_sex_vecs[nn_mi]    = ind_class["sex_num"].to_numpy()
-    pred_sex_vecs[nn_mi]   = ind_class["class_num"].to_numpy()
+        true_sex_vecs[nn_mi]    = ind_class["sex_num"].to_numpy()
+        pred_sex_vecs[nn_mi]   = ind_class["class_num"].to_numpy()
 
-avg_agreement_dict = {}
+    avg_agreement_dict = {}
 
-for nn in range(1, 11):  # 1..10
-    v = pred_sex_vecs[nn]
+    for nn in nn_values:  # 1..10
+        v = pred_sex_vecs[nn]
 
-    sim_sum = 0.0
-    n_other = 0
+        sim_sum = 0.0
+        n_other = 0
 
-    for nn2 in range(1, 11):
-        if nn2 == nn:
-            continue
+        for nn2 in nn_values:
+            if nn2 == nn:
+                continue
 
-        v2 = pred_sex_vecs[nn2]
+            v2 = pred_sex_vecs[nn2]
 
-        # similarity = fraction of equal entries
-        sim = np.mean(v == v2)
+            # similarity = fraction of equal entries
+            sim = np.mean(v == v2)
 
-        sim_sum += sim
-        n_other += 1
+            sim_sum += sim
+            n_other += 1
 
-    avg_agreement_dict[nn] = sim_sum / n_other  # n_other should be 9
+        avg_agreement_dict[nn] = sim_sum / n_other  # n_other should be 9
 
-df_avg = pd.DataFrame(
-    {
-        "nn_mi": list(avg_agreement_dict.keys()),
-        "avg_agreement": list(avg_agreement_dict.values()),
-    }
-).sort_values("nn_mi")
+    df_avg = pd.DataFrame(
+        {
+            "nn_mi": list(avg_agreement_dict.keys()),
+            "avg_agreement": list(avg_agreement_dict.values()),
+        }
+    ).sort_values("nn_mi")
 
-out_file = f"{results_out_path}/Stability_nn_mi.csv"
+    out_file = f"{results_out_path}/Stability_nn_mi.csv"
 
-df_avg.to_csv(out_file, index=False)
+    df_avg.to_csv(out_file, index=False)
+
+    avg_neigh_agree_dict = {}
+
+    for i in range(1, len(nn_values) - 1):
+        left = nn_values[i - 1]
+        curr  = nn_values[i]
+        right = nn_values[i + 1]
+
+        sim = (np.mean(pred_sex_vecs[curr] == pred_sex_vecs[left]) + np.mean(pred_sex_vecs[curr] == pred_sex_vecs[right]))/2
+        avg_neigh_agree_dict[curr] = sim
+
+    df_neigh_avg = pd.DataFrame(
+        {
+            "nn": list(avg_neigh_agree_dict.keys()),
+            "avg_agreement": list(avg_neigh_agree_dict.values()),
+        }
+    ).sort_values("nn")
+
+    out_file = f"{results_out_path}/Stability_neighbour_nn_mi.csv"
+
+    df_neigh_avg.to_csv(out_file, index=False)
 
 # pairwise agreement
 #agree_3_5 = (labels_3 == labels_5).mean()
@@ -78,3 +99,7 @@ df_avg.to_csv(out_file, index=False)
 # If all stab_nn are very similar and high (e.g. > 0.95), just pick the smallest nn (3) or stick 
 # with the default for simplicity and report that results are robust across 
 
+
+# Execute script
+if __name__ == "__main__":
+    main()
--- run_results_summary.py
+++ run_results_summary.py
@@ -8,10 +8,18 @@ from sklearn.preprocessing import StandardScaler
 import socket
 
 import _results_1_summarize_classifications
-# import _1b_sTOPF_loo_PCA
+import _results_2_assess_stability
+import _results_3_assess_stability_corr
+
 
 base_path =  "/Users/sweis/Data/Arbeit/Juseless/data/project/brainvar_sexdiff_movies" 
-results = "results_run_sTOPF_v2_data_v2"
-nn = nn_values = [1,2,3,4,5,6,7,8,9,10,15]
 
-_results_1_summarize_classifications.main(base_path,results,nn)
+#results = "results_run_sTOPF_v2"
+#nn_values = [3,5,10,15,20,25,30,35,40,50,60,70,80,90,100]
+
+results = "results_run_sTOPF_v2_data_v2"
+nn_values = [1,2,3,4,5,6,7,8,9,10]
+
+#_results_1_summarize_classifications.main(base_path,results,nn)
+_results_2_assess_stability.main(base_path,results,nn_values)
+_results_3_assess_stability_corr.main(base_path,results)
