a595b6be0a8cd5ad027694e71412bba46768bd80
Susanne Weis <s.weis@fz-juelich.de>
2026-02-16 14:06:55 +0100

    _10d added quantile brains

--- _10d_archetype_brains_UMAP_HDBSCAN.py
+++ _10d_archetype_brains_UMAP_HDBSCAN.py
@@ -1,13 +1,24 @@
 import pandas as pd
 import numpy as np
 from _util_glass_brains import create_glassbrains
+import os 
 
 
 
 def main(base_path, proj, nn_mi,movies_properties):
 
     results_path = f"{base_path}/results_run_sTOPF_v2_data_{proj}/results_nn{nn_mi}"
+
     arch_path = f"{results_path}/archetype_clusters_UMAP_HDBSCAN"
+    arch_movie_path = f"{results_path}/archetype_clusters_UMAP_HDBSCAN/perMovie"
+    arch_movie_glass_path = f"{results_path}/archetype_clusters_UMAP_HDBSCAN/perMovie/glassbrains"
+    arch_movie_glass_quant_path = f"{results_path}/archetype_clusters_UMAP_HDBSCAN/perMovie/glassbrains/quantiles"
+
+
+    os.makedirs(arch_movie_path, exist_ok=True) # Create the output directory if it doesn't exist
+    os.makedirs(arch_movie_glass_path, exist_ok=True) # Create the output directory if it doesn't exist
+    os.makedirs(arch_movie_glass_quant_path, exist_ok=True) # Create the output directory if it doesn't exist
+
 
     movies = list(movies_properties.keys())
  
@@ -34,13 +45,13 @@ def main(base_path, proj, nn_mi,movies_properties):
             summary = (
                 df.groupby(["cluster", "region"])
                 .agg(
-                    mean_corr=(column, "mean"),
-                    var_corr=(column, "var"),
+                    mean = (column, "mean"),
+                    var = (column, "var"),
                 )
                 .reset_index()
             )
 
-            out_path = f"{arch_path}/Archetype_{curr_mov}_brain_UMAP_HDBSCAN_{metric}.csv"
+            out_path = f"{arch_movie_path}/Archetype_{curr_mov}_brain_UMAP_HDBSCAN_{metric}.csv"
             summary.to_csv(out_path, index=False)
 
             all_clust_data = pd.read_csv(out_path)
@@ -49,19 +60,47 @@ def main(base_path, proj, nn_mi,movies_properties):
             # sep files per cluster and glass brains    
             for c in clusters:
                 df_c = all_clust_data[all_clust_data["cluster"] == c].copy()
-                out_sep = f"{arch_path}/Archetype_{curr_mov}_brain_UMAP_HDBSCAN_{metric}_cluster-{c}.csv"
+
+                q25 = df_c["mean"].quantile(0.25)
+                q75 = df_c["mean"].quantile(0.75)
+                q10 = df_c["mean"].quantile(0.10)
+                q90 = df_c["mean"].quantile(0.90)
+
+
+                # Add quantile flag column
+                df_c["Quantile"] = np.select(
+                    [
+                        df_c["mean"] <= q10,
+                        df_c["mean"] >= q90
+                    ],
+                    [-1, 1],
+                    default=0
+                )
+
+                out_sep = f"{arch_movie_path}/Archetype_{curr_mov}_brain_UMAP_HDBSCAN_{metric}_cluster-{c}.csv"
                 df_c.to_csv(out_sep, index=False)
 
-                roi_fill_name = "mean_corr"
-                roi_value_name = "region"
                 data_path = f"{base_path}/data_run_sTOPF_{proj}"
                 atlas_path = f"{data_path}/Susanne_Schaefer_436.nii"
                 roi_name_file = f"{data_path}/ROI_names.csv"
                 roi_names = pd.read_csv(roi_name_file)["roi_name"].tolist()
+
+                roi_fill_name = "mean"
+                roi_value_name = "region"
                 title = f"Brain Archetype UMAP HDBSCAN Cluster {c} {curr_mov} {metric}"
                 name_str = f"Brain_Archetype_UMAP_HDBSCAN_Cluster_{c}_{curr_mov}_{metric}"
                 
-                create_glassbrains(out_sep, roi_fill_name, roi_value_name, roi_names, atlas_path, title, arch_path, name_str,"continuous")
+                create_glassbrains(out_sep, roi_fill_name, roi_value_name, roi_names, atlas_path, title, arch_movie_glass_path, name_str,"continuous")
+                
+                # Quantile brains
+
+                roi_fill_name = "Quantile"
+                roi_value_name = "region"
+                title = f"Brain Archetype Quantiles UMAP HDBSCAN Cluster {c} {curr_mov} {metric}"
+                name_str = f"Brain_Archetype_UMAP_HDBSCAN_Cluster_{c}_{curr_mov}_{metric}_quantiles"
+
+                create_glassbrains(out_sep, roi_fill_name, roi_value_name, roi_names, atlas_path, title, arch_movie_glass_quant_path, name_str,"discrete")
+
 
 
 # Execute script
