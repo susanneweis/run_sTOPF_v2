f3eff4218a3ea50afacad2a9f1f8b4095e7a6980
Susanne Weis <s.weis@fz-juelich.de>
2026-03-06 12:53:14 +0100

    minor changes

--- run_results_summary.py
+++ run_results_summary.py
@@ -12,6 +12,7 @@ import _results_2_assess_stability
 import _results_3_assess_stability_corr
 import _results_4_sex_type_distributions
 import _results_5_CV_classification_results
+import _results_6_classification_results
 
 
 base_path =  "/Users/sweis/Data/Arbeit/Juseless/data/project/brainvar_sexdiff_movies" 
@@ -28,12 +29,18 @@ nn_values = [11]
 #_results_3_assess_stability_corr.main(base_path,results)
 #_results_4_sex_type_distributions.main(base_path,results)
 
-for nn in nn_values:        
-    path = f"{base_path}/{results}/results_nn{nn}/ind_classification_CV/"
-    file = "classification_CV_corr"
-    col = "cv_balanced_accuracy_mean"
-    _results_5_classification_results.main(path,file,col)
-    path = f"{base_path}/{results}/results_nn{nn}/ind_classification_CV/"
-    file = "classification_CV_corr"
-    col = "test_balanced_accuracy"
-    _results_5_CV_classification_results.main(path,file,col)
+# for nn in nn_values:        
+#     path = f"{base_path}/{results}/results_nn{nn}/ind_classification_CV/"
+#     file = "classification_CV_corr"
+#     col = "cv_balanced_accuracy_mean"
+#     _results_5_classification_results.main(path,file,col)
+#     path = f"{base_path}/{results}/results_nn{nn}/ind_classification_CV/"
+#     file = "classification_CV_corr"
+#     col = "test_balanced_accuracy"
+#     _results_5_CV_classification_results.main(path,file,col)
+
+for nn in nn_values: 
+    path = f"{base_path}/{results}"
+    file = "mean_classification correct_by_movie_corr_perc_LONG"
+    col = "mean_classification correct"
+    _results_6_classification_results.main(path,file,col)
